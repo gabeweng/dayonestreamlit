c989993c6297508461cc65d531c9084ba3faa37f
Anderson Seo <Ayseo102007@gmail.com>
2023-11-26 12:20:02 -0500

    iimage fix???

--- pages/anderseo.py
+++ pages/anderseo.py
@@ -3,4 +3,4 @@ import pandas as pd
 import numpy as np
 
 st.title('Anderson')
-st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.polygon.com%2F23150011%2Fpokemon-violet-scarlet-smoliv-memes-fan-art-gen-9&psig=AOvVaw1yGZZGYv8aEJ1bP0GcQjOq&ust=1701104746408000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOj3n5OT4oIDFQAAAAAdAAAAABBP')
+st.image('https://cdn.vox-cdn.com/thumbor/HFdhANVIrxsJzT5cv9U5n2cH5tk=/0x0:960x540/1200x800/filters:focal(404x194:556x346)/cdn.vox-cdn.com/uploads/chorus_image/image/70931916/p16_01_m.0.jpg')
