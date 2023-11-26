af3fd909b354943342200923dacfa5523397b17c
Gunnar Eklund <105998814+gweklund711@users.noreply.github.com>
2023-11-26 12:06:53 -0500

    Update gunnar.py
    fixed a minor error

--- pages/gunnar.py
+++ pages/gunnar.py
@@ -6,5 +6,5 @@ st.title('Gunnar')
 
 st.image('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024')
 st.write("Never gonna give you up")
-st.write("Never gonna let you down)
+st.write("Never gonna let you down")
 st.write("Never gonna run around and desert you")
