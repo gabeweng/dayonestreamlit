7e043157daec0a5edc675c856f3230fa2f9c23d1
Gunnar Eklund <105998814+gweklund711@users.noreply.github.com>
2023-11-26 11:57:27 -0500

    Update gunnar.py

--- pages/gunnar.py
+++ pages/gunnar.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Gabe')
+st.title('Gunnar')
 
 st.image('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024')
