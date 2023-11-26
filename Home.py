b94bc3999095a05a92ce9b4130ab91cc754e8fd7
gabeweng <gabeweng@gmail.com>
2023-11-26 11:30:22 -0500

    added home.py

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Home')
+
+st.write('Welcome to the home page of the app. This is where you can find the data and the code used to create the app.')
