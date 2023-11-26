f4b6877832724ecf2b71bad929138868aa59cafe
Anderson Seo <Ayseo102007@gmail.com>
2023-11-26 12:16:41 -0500

    image fix

--- pages/anderseo.py
+++ pages/anderseo.py
@@ -3,4 +3,4 @@ import pandas as pd
 import numpy as np
 
 st.title('Anderson')
-st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pokemon.com%2Fus%2Fpokedex%2Fsmoliv&psig=AOvVaw1yGZZGYv8aEJ1bP0GcQjOq&ust=1701104746408000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOj3n5OT4oIDFQAAAAAdAAAAABAE')
+st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.polygon.com%2F23150011%2Fpokemon-violet-scarlet-smoliv-memes-fan-art-gen-9&psig=AOvVaw1yGZZGYv8aEJ1bP0GcQjOq&ust=1701104746408000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOj3n5OT4oIDFQAAAAAdAAAAABBP')
