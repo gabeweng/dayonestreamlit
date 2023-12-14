aacabeb23e03721f96c886f280f4a19e3b14b82c
JT Penny <dabomb07@icloud.com>
2023-12-14 14:59:02 -0500

    init jt

--- Home.py
+++ Home.py
@@ -7,3 +7,7 @@ st.title('Home')
 st.write('Welcome to the home page of the app. This is where you can find the data and the code used to create the app.')
 
 st.header('I LOVE CODING BECAUSE IT IS FUN!')
+
+
+st.write('awesome')
+st.video('./https://media.tenor.com/E3FzOl_zIeYAAAAC/funee-monkee.gif')
